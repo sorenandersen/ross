f5e36274a5db56675be76068aa24e7c0a8455a01
Soren Andersen <mail@sorenandersen.com>
2020-12-22 18:23:07 +0100

    ebProcessNewUser test finalized.

--- services/rest-api/tests/integration/process-new-user.spec.ts
+++ services/rest-api/tests/integration/process-new-user.spec.ts
@@ -1,9 +1,7 @@
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
-import { AWS_REGION, cognitoConfig, ddbConfig } from '@svc/config';
+import { AWS_REGION, cognitoConfig } from '@svc/config';
 import { TestUserManager } from '@tests/utils/test-user-manager';
 import { getUser, deleteUser } from '@svc/lib/repos/ross-repo';
 
-const ddb = new DocumentClient({ region: AWS_REGION });
 const userManager = new TestUserManager({
   cognitoUserPoolId: cognitoConfig.userPoolId,
   cognitoUserPoolClientId: cognitoConfig.staffUserPoolClientId,
@@ -19,26 +17,19 @@ describe('`ebProcessNewUser` Lambda function', () => {
     createdUserIds = [];
   };
 
-  it.todo(
-    'a UserCreatedEvent in EventBridge should save the user profile to DynamoDB',
-  );
-
   it('a confirmed user sign up in Cognito should save the user profile to DynamoDB', async () => {
-    // Arrange: Create and confirm Cognito user
+    // Arrange:
+    // No arrangement to be done; createUser call kicks off the flow
+
+    // Act: Create and confirm Cognito user
     // Keep track of id's of created users
     const userContext = await userManager.createUser();
     createdUserIds.push(userContext.id);
 
-    // const response = await ddb
-    //   .get({
-    //     TableName: ddbConfig.usersTable,
-    //     Key: {
-    //       id: userContext.id,
-    //     },
-    //   })
-    //   .promise();
-    // const ddbUser = response.Item as User | undefined;
+    // Sleep, to account for slight EventBridge delay
+    await new Promise((r) => setTimeout(r, 1000));
 
+    // Assert
     const ddbUser = await getUser(userContext.id);
 
     expect(ddbUser).toBeTruthy();
@@ -47,6 +38,6 @@ describe('`ebProcessNewUser` Lambda function', () => {
   });
 
   afterAll(async () => {
-    //await Promise.all([userManager.dispose(), deleteDdbUsers()]);
+    await Promise.all([userManager.dispose(), deleteDdbUsers()]);
   });
 });
