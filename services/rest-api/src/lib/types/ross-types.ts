88446c604aed44d5a195e0f9a1cd2e749f579d01
Soren Andersen <mail@sorenandersen.com>
2020-12-23 16:40:02 +0100

    Restaurant type added.

--- services/rest-api/src/lib/types/ross-types.ts
+++ services/rest-api/src/lib/types/ross-types.ts
@@ -1,3 +1,19 @@
+export enum Region {
+  MANHATTAN = 'MANHATTAN',
+  BRONX = 'BRONX',
+  FOO = 'FOO',
+  BAR = 'BAR',
+  TODO = 'TODO',
+}
+
+// ==== Users
+
+export enum UserRole {
+  MANAGER = 'MANAGER',
+  STAFF = 'STAFF',
+  CUSTOMER = 'CUSTOMER',
+}
+
 export interface UserProfile {
   username: string;
   email: string;
@@ -6,6 +22,28 @@ export interface UserProfile {
 
 export interface User extends UserProfile {
   id: string;
+}
+
+// ==== Restaurants
+
+export enum RestaurantAvailability {
+  PRIVATE = 'PRIVATE',
+  PUBLIC = 'PUBLIC',
+}
+
+export enum RestaurantApprovalStatus {
+  //PENDING_APPROVAL = 'PENDING_APPROVAL', // For a future version, a RossAdmin should approve new restaurants before managers can operate on them.
+  APPROVED = 'APPROVED',
+}
+
+export interface Restaurant {
+  id: string;
+  name: string;
+  description: string;
+  availability: RestaurantAvailability;
+  region: Region;
+  profilePhotoUrl: string;
+  approvalStatus: RestaurantApprovalStatus;
 }
 
 // ==== EventBridge event message schemas
