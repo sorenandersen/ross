71155d24fb03b198f3764fdb6752c5f9852a1f35
Soren Andersen <mail@sorenandersen.com>
2021-01-10 11:17:45 +0100

    VS Code debugging via test suites setup

---
+++ services/rest-api/tests/unit/temp.spec.ts
@@ -0,0 +1,8 @@
+describe('temp unit test suite', () => {
+  it('is a temp test to prove unit test setup', () => {
+    const i = 1;
+    const j = 1;
+    const result = i + j;
+    expect(result).toBe(2);
+  });
+});
