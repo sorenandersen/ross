a81a4b469afc40be2974a334e2f3754a7453623b
Soren Andersen <mail@sorenandersen.com>
2021-01-15 00:39:40 +0100

    Middleware 'apigw-error-handler' work-in-progress

--- services/rest-api/src/handlers/http/restaurants/get.ts
+++ services/rest-api/src/handlers/http/restaurants/get.ts
@@ -1,13 +1,15 @@
 import log from '@dazn/lambda-powertools-logger';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import createError from 'http-errors';
 import { getUserFromClaims } from '@svc/lib/auth/claims-parser';
 import { getRestaurant } from '@svc/lib/repos/ross-repo';
 import { Restaurant, RestaurantVisibility } from '@svc/lib/types/ross-types';
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { wrap } from '@svc/lib/middleware/apigw-error-handler';
 
 /**
  * Gets a specific restaurant
  */
-export const handler = async (event: APIGatewayProxyEventV2) => {
+export const handler = wrap(async (event: APIGatewayProxyEventV2) => {
   log.debug(
     `${event.requestContext?.http?.method?.toUpperCase()} /restaurants/${
       event.pathParameters?.id
@@ -24,10 +26,7 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
     // **
     restaurant = await getRestaurant(restaurantId);
     if (!restaurant || restaurant.visibility !== RestaurantVisibility.PUBLIC) {
-      return {
-        statusCode: 404,
-        body: '',
-      };
+      throw new createError.NotFound();
     }
   } else {
     // **
@@ -35,10 +34,7 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
     // **
     // Validate that equested restaurant is associated with requesting user
     if (user.restaurantId !== restaurantId) {
-      return {
-        statusCode: 403,
-        body: JSON.stringify({}),
-      };
+      throw new createError.Forbidden();
     }
   }
 
@@ -49,4 +45,4 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
     statusCode: 200,
     body: JSON.stringify({ ...restaurant }),
   };
-};
+});
---
+++ services/rest-api/src/lib/middleware/apigw-error-handler.ts
@@ -0,0 +1,64 @@
+import log from '@dazn/lambda-powertools-logger';
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResult,
+  //APIGatewayProxyResultV2,
+  Context,
+} from 'aws-lambda';
+
+export type PromiseHandler = (
+  event: APIGatewayProxyEventV2,
+  context: Context,
+) => Promise<APIGatewayProxyResult>;
+
+export const wrap = (handler: PromiseHandler): PromiseHandler => async (
+  event: APIGatewayProxyEventV2,
+  context: Context,
+): Promise<APIGatewayProxyResult> => {
+  try {
+    return await handler(event, context);
+  } catch (error) {
+    const statusCodeError =
+      error.statusCode && typeof error.statusCode === 'number';
+
+    if (statusCodeError && error.statusCode < 500) {
+      // **
+      // HTTP status code error
+      // **
+      log.info('statusCodeError', {
+        statusCode: error.statusCode,
+        error,
+        event,
+        context,
+      });
+
+      // Reveal error as-is
+      return {
+        statusCode: error.statusCode,
+        body: JSON.stringify({
+          error: error.message,
+        }),
+      };
+    }
+
+    // **
+    // Non-HTTP or server error
+    // Log and return nondisclosed error
+    // **
+    log.error(
+      'Unhandled error',
+      {
+        event,
+        context,
+      },
+      error,
+    );
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: 'Internal Server Error',
+      }),
+    };
+  }
+};
