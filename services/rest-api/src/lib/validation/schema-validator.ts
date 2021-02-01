04d041b02576909bfc101cd560be49182bdcb520
Soren Andersen <mail@sorenandersen.com>
2021-02-01 21:54:03 +0100

    Schema id added. Ajv init arg 'schemaId' corrected.

--- services/rest-api/src/lib/validation/schema-validator.ts
+++ services/rest-api/src/lib/validation/schema-validator.ts
@@ -1,7 +1,7 @@
 import Ajv from 'ajv';
 
 export const validate = (schema: object, data: object) => {
-  const ajv = new Ajv({ schemaId: 'id', allErrors: true });
+  const ajv = new Ajv({ schemaId: 'auto', allErrors: true });
   ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
   const valid = ajv.validate(schema, data);
   const errorsText =
