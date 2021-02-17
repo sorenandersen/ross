7db139a89b38fa788d8b16cb93b4c35b34e09cd0
Soren Andersen <mail@sorenandersen.com>
2021-02-17 17:55:10 +0100

    PATCH /restaurants/{restaurantId}/seatings/{seatingId}/decline done

---
+++ services/rest-api/src/handlers/http/seatings/decline.ts
@@ -0,0 +1,64 @@
+import log from '@dazn/lambda-powertools-logger';
+import { wrap } from '@svc/lib/middleware/apigw-error-handler';
+import createError from 'http-errors';
+import { errorMessages } from '@svc/lib/validation/messages';
+import { getUserFromClaims } from '@svc/lib/auth/claims-parser';
+import { getSeating, updateSeatingStatus } from '@svc/lib/repos/ross-repo';
+import { SeatingStatus } from '@svc/lib/types/ross-types';
+
+/**
+ * Updates seating status to DECLINED
+ */
+export const handler = wrap(async (event) => {
+  const restaurantId = event.pathParameters?.restaurantId!;
+  const seatingId = event.pathParameters?.seatingId!;
+
+  log.debug(
+    `${event.requestContext?.http?.method?.toUpperCase()} ${
+      event.rawPath
+    } called`,
+    { event },
+  );
+
+  // Validate that StaffUser is requesting a resource for his associated restaurant
+  const user = getUserFromClaims(event.requestContext.authorizer?.jwt.claims!);
+  if (user.restaurantId !== restaurantId) {
+    throw new createError.Forbidden(errorMessages.forbidden);
+  }
+
+  // Fetch seating and validate that it is associated with restaurant
+  const seating = await getSeating(seatingId, restaurantId);
+  if (!seating) {
+    throw new createError.NotFound(errorMessages.notFound);
+  }
+  if (seating.restaurantId !== restaurantId) {
+    throw new createError.Forbidden(errorMessages.forbidden);
+  }
+
+  // Validate against current seating status
+  switch (seating.status) {
+    case SeatingStatus.PENDING:
+      // Seating meets the requirement for being declined
+      // Perform update
+      await updateSeatingStatus(
+        seatingId,
+        restaurantId,
+        SeatingStatus.DECLINED,
+      );
+      break;
+    case SeatingStatus.DECLINED:
+      // Seating already declined - no actions needed
+      break;
+    default:
+      // Invalid state for operation
+      // Signal error
+      throw new createError.Conflict(
+        'Operation not allowed for seating in current status',
+      );
+  }
+
+  return {
+    statusCode: 204,
+    body: '',
+  };
+});
